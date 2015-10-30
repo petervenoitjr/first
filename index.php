88b6e34264d6b57099654a9272b741313af5f29d
Peter <petervenoitjr@gmail.com>
2015-10-29 21:33:49 -0400

    my first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+$first = "Works?";
+?>
+ <!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+<title>Peter's Page</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+<p><?php
+echo $first;
+?></p>
+<h2>Another!!!!</h2>
+
+</body>
+</html> 
